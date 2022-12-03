bf0e1f82067e5773c858b6488a3d4fe002f7a8a3
Mauricio Soares <10080452@yntcd01.int>
2022-12-02 22:36:50 -0300

    linha teste inserida

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int i = 11;
+do
+{
+    
+    Console.WriteLine(i);
+    //teste
+    Console.WriteLine(i);
+} while(i<=10);
+
+
+
+
